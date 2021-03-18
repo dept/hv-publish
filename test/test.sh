e6642458b84e0b1acb233b506397ea4938cc457b
Severin Klaus <sk@hinderlingvolkart.com>
2021-03-18 13:57:37 +0100

    Adds test

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,22 @@
+#!/bin/bash
+
 # Set some env variables
-BB_AUTH_STRING=
+export $(grep -v '^#' .env | xargs)
 
-cd hv-publish-testbuild
-node build.js
-cd ..
-
-node test.js
+rm -rf hv-publish-testbuild
+rm -rf repo
 
 # Clone a bitbucket repository
+# let's get our small test project
+git clone https://$BB_AUTH_STRING@bitbucket.org/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}.git
+
+# build the test project (changes, deletes, adds some files)
+cd hv-publish-testbuild
+node build.js
+
 
 # save2repo
+../../bin/save2repo
 
 # hv-publish
+../../bin/hv-publish.js
