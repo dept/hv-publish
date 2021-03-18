973b559b78b1eb26b78ea8523f584731b06b1081
Severin Klaus <sk@hinderlingvolkart.com>
2021-03-18 09:37:43 +0100

    Updated readme, move publish to lib

--- Readme.md
+++ Readme.md
@@ -4,6 +4,74 @@ HV Publish
 Within bitbucket pipelines:
 
 ```
-npm install -g git+https://git@bitbucket.org/hinderlingvolkart/hv-publish.git
+npm install -g hv-publish
 hv-publish
 ```
+
+
+## Combination Strategy (`hv-publish` and `save2repo`)
+
+`hv-publish` and `save2repo` are very often used in conjunction, but the order may vary:
+
+    - hv-publish
+    - save2repo
+
+or
+
+    - save2repo
+    - hv-publish
+
+The first should be the normal case. But whatever the order, we want to get most out of the combination:
+
+`hv-publish` should
+- publish to Netlify
+- save the website info to hv.dev
+- save the commit info to hv.dev
+    - of the source repository
+    - and the build repository
+
+`save2repo` should
+- commit to another bitbucket git repository
+    - the commit message should include the hv.dev index + commit messages
+
+To make sure this works for both order cases, let's take these steps:
+
+### `hv-publish`, then `save2repo`
+
+- hv-publish
+    - publish to Netlify
+    - save to hv.dev --> `.hv-publish.json`
+- save2repo
+    - save to repo, use info of `.hv-publish.json` --> .save2repo.json
+    - patch hv.dev
+
+### `save2repo`, then `hv-publish`
+
+- save2repo
+    - save to repo, use only commit messages --> `.save2repo.json`
+- hv-publish
+    - publish to Netlify
+    - save to hv.dev, use info of `.save2repo.json`
+    - update commit message with info of hv-publish
+
+### Combined
+
+- hv-publish
+    - publish to Netlify
+    - if `.save2repo.json`
+        - save to hv.dev with all info
+        - update commit message with info from hv.dev
+    - else
+        - save to hv.dev without build_repo info
+        - save result to `.hv-publish.json`
+
+- save2repo
+    - if `.hv-publish.json`
+        - commit to repo with info from `.hv-publish.json`
+        - patch hv.dev with commit info
+    - else
+        - commit to repo without hv.dev info (no index)
+        - save commit info to `.save2repo.json`
+
+
+
--- bin/hv-publish.js
+++ bin/hv-publish.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-const Color = require('ansi-colors');
+const Color = require('ansi-colors')
 
-const parseArgs = require('minimist');
+const parseArgs = require('minimist')
 
-const package = require('../package.json');
-const publish = require('../index.js');
+const package = require('../package.json')
+const publish = require('../lib/publish.js')
 
-console.log(Color.white.bgBlue.bold('HV Publish'), Color.yellow.bgBlue.bold(`v${package.version}`));
+console.log(Color.white.bgBlue.bold('HV Publish'), Color.yellow.bgBlue.bold(`v${package.version}`))
 
 // Parsing Arguments
 // =================
@@ -36,56 +36,56 @@ const ARGS = Object.assign(
 		project: process.env.BITBUCKET_REPO_SLUG,
 		name: null, // the netlify site name base – defaults to project (only for compatibility)
 		site: null,
-		version: null
+		version: null,
 	},
 	parseArgs(process.argv, {
 		alias: {
 			s: 'source',
 			n: 'name',
 			p: 'project',
-			v: 'version'
-		}
+			v: 'version',
+		},
 	})
-);
+)
 
-let hasInputError = false;
+let hasInputError = false
 
 if (ARGS.version) {
-	console.log(`HV Publish Version: ${package.version}`);
-	process.exit();
+	console.log(`HV Publish Version: ${package.version}`)
+	process.exit()
 }
 if (!ARGS.netlify) {
-	console.log(Color.red('- Missing: netlify or $NETLIFY_ACCESS_TOKEN'));
-	hasInputError = true;
+	console.log(Color.red('- Missing: netlify or $NETLIFY_ACCESS_TOKEN'))
+	hasInputError = true
 }
 if (!ARGS.hvify) {
-	console.log(Color.red('- Missing: hvify or $HVIFY_TOKEN'));
-	hasInputError = true;
+	console.log(Color.red('- Missing: hvify or $HVIFY_TOKEN'))
+	hasInputError = true
 }
 if (!ARGS.commit) {
-	console.log(Color.red('- Missing: commit or $BITBUCKET_COMMIT'));
-	hasInputError = true;
+	console.log(Color.red('- Missing: commit or $BITBUCKET_COMMIT'))
+	hasInputError = true
 }
 if (!ARGS.branch) {
-	console.log(Color.red('- Missing: branch or $BITBUCKET_BRANCH'));
-	hasInputError = true;
+	console.log(Color.red('- Missing: branch or $BITBUCKET_BRANCH'))
+	hasInputError = true
 }
 if (!ARGS.project) {
-	console.log(Color.red('- Missing: project or $BITBUCKET_REPO_SLUG'));
-	hasInputError = true;
+	console.log(Color.red('- Missing: project or $BITBUCKET_REPO_SLUG'))
+	hasInputError = true
 }
 if (!ARGS.name) {
-	ARGS.name = `${ARGS.project}`;
+	ARGS.name = `${ARGS.project}`
 }
 if (!ARGS.site) {
-	ARGS.sitename = `hv-${ARGS.name}`;
-	ARGS.site = `hv-${ARGS.name}.netlify.com`;
+	ARGS.sitename = `hv-${ARGS.name}`
+	ARGS.site = `hv-${ARGS.name}.netlify.com`
 }
 if (hasInputError) {
-	console.dir(ARGS, { colors: true });
-	process.exit();
+	console.dir(ARGS, { colors: true })
+	process.exit()
 }
 
 publish(ARGS)
 	.then(() => console.log('done.'))
-	.catch(error => console.log(error));
+	.catch((error) => console.log(error))
